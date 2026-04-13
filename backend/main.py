2afd50161f5fdcfc6921dc3d6f253cf5602ca793
Rishi009 <saunishpal@gmail.com>
2026-04-13 12:21:53 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://https://digital-marketing-agent.netlify.app"
+        "https://digital-marketing-agent.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
